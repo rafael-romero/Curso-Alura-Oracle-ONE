5547111e35695ee122c3faca2128f49be8983dc1
Rafael Romero <rafa710@gmail.com>
2022-09-13 01:10:19 -0300

    curso- JAVA parte1

---
+++ Back-End/JAVA/Java parte1-primeros pasos/practica-curso-primeros-pasos/java-primeros-pasos/src/TipoVariable.java
@@ -0,0 +1,18 @@
+
+public class TipoVariable {
+
+	public static void main(String[] args) {
+		
+		double variable1 = 230.89;
+		int variable1Entero = (int)variable1;// Cast
+		// este metodo imprime en consola
+		System.out.println(variable1Entero);
+		
+		 long prueba = 1222222222200L;
+		 
+		 int resultado = (int) variable1 + variable1Entero;
+		 System.out.println(resultado);
+		 
+	}
+	
+}
